c2ddd110d650ed730ad78549d579b90c4915de52
ialnezami <ibrahim.alnezami@gmail.com>
2023-04-01 10:28:31 +0200

    ✨ feat: add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 import { ItemModule } from './item/item.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule as Conf } from '@nestjs/config';
 import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [CategoryModule, ItemModule, UserModule, ConfigModule],
+  imports: [
+    ConfigModule,
+    CategoryModule,
+    ItemModule,
+    UserModule,
+    Conf.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
